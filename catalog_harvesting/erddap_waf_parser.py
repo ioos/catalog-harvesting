f449f3e2a4adeddd0164bfe059409c161671c91d
Benjamin Adams <ben.adams@rpsgroup.com>
2018-08-02 17:24:52 -0400

    Conditional handling for ERDDAP WAFs
    Determines the ERDDAP version running by scraping the HTML, then adds
     conditional handling to fetch ERDDAP WAF links depending on the version
     running:
     version < 1.82: Fetches links from <pre> element
     version >= 1.82: Fetches links from <table> inside <div> element

--- catalog_harvesting/erddap_waf_parser.py
+++ catalog_harvesting/erddap_waf_parser.py
@@ -5,6 +5,9 @@ catalog_harvesting/waf_parser.py
 
 from catalog_harvesting.waf_parser import WAFParser
 from bs4 import BeautifulSoup
+import six
+import re
+from distutils.version import LooseVersion
 
 
 class ERDDAPWAFParser(WAFParser):
@@ -15,7 +18,24 @@ class ERDDAPWAFParser(WAFParser):
         '''
         retval = []
         soup = BeautifulSoup(content, 'html.parser')
-        for link in soup.find('pre').find_all('a'):
+	raw_ver = soup.find(text=re.compile('ERDDAP, Version .*$'))
+        # could likely equivalently check for None here
+        if not isinstance(raw_ver, six.string_types):
+           ver_full = None
+        else:
+	   try:
+	       ver_full = LooseVersion(raw_ver.strip().rsplit()[-1])
+           except:
+           # TODO: add warnings
+               ver_full = None
+
+        if ver_full is None or ver_full < LooseVersion('1.82'):
+            # if the ERDDAP version is less than 1.82, the attributes are stored in a <pre>
+            link_container = soup.find('pre')
+        else:
+            link_container = soup.find('div', {'class': 'standard_width'}).find('table')
+        
+        for link in link_container.find_all('a'):
             if link.text.endswith('.xml'):
                 retval.append((link.get('href'), link.text))
         return retval
