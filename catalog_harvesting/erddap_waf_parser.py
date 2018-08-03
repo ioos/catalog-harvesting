a39883cd52fe2674ad3de27a335aa77aabee4e08
Benjamin Adams <ben.adams@rpsgroup.com>
2018-08-03 11:55:51 -0400

    Fixup for error conditions, remove re
    Removes re module in favor of using str `in` to detect ERDDAP version.
    
    Adds function to prevent failure if text is None when scanning for text
    nodes ending in '.xml'
    
    Fixes a couple indentation issues.

--- catalog_harvesting/erddap_waf_parser.py
+++ catalog_harvesting/erddap_waf_parser.py
@@ -6,7 +6,6 @@ catalog_harvesting/waf_parser.py
 from catalog_harvesting.waf_parser import WAFParser
 from bs4 import BeautifulSoup
 import six
-import re
 from distutils.version import LooseVersion
 
 
@@ -16,17 +15,16 @@ class ERDDAPWAFParser(WAFParser):
         '''
         Returns a list of tuples href, text for each anchor in the document
         '''
-        retval = []
         soup = BeautifulSoup(content, 'html.parser')
-	raw_ver = soup.find(text=re.compile('ERDDAP, Version .*$'))
+        raw_ver = soup.find(text=lambda t: 'ERDDAP, Version ' in t)
         # could likely equivalently check for None here
         if not isinstance(raw_ver, six.string_types):
-           ver_full = None
+            ver_full = None
         else:
-	   try:
-	       ver_full = LooseVersion(raw_ver.strip().rsplit()[-1])
-           except:
-           # TODO: add warnings
+            try:
+               ver_full = LooseVersion(raw_ver.strip().rsplit()[-1])
+            except:
+            # TODO: add warnings
                ver_full = None
 
         if ver_full is None or ver_full < LooseVersion('1.82'):
@@ -34,9 +32,6 @@ class ERDDAPWAFParser(WAFParser):
             link_container = soup.find('pre')
         else:
             link_container = soup.find('div', {'class': 'standard_width'}).find('table')
-        
-        for link in link_container.find_all('a'):
-            if link.text.endswith('.xml'):
-                retval.append((link.get('href'), link.text))
-        return retval
 
+        return [(link.get('href'), link.text) for link in
+                link_container.find_all('a', text=lambda t: t.endswith('.xml'))]
