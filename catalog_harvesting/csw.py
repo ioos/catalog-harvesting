ioos/catalog-harvesting

commit 92394ea609db374175159407cbf910214f0e804c

Fixes "infinite" loop in CSW harvest
Removes an "infinite" (actually limited by max_batches parameter) loop in the
CSW harvesting code.  nextRecord is set to 0 when all the records have
been fetched, but the code was checking if nextRecord-1 was equal to the
number of matched records, which appeared to be unsatisfiable.  Thus,
redundant fetches were made until the maximum batches were exceeded.

catalog_harvesting/csw.py
@@ -27,19 +27,20 @@ def get_records(csw, max_batches=10000):
 
     # set a maximum number of record batches just as a precaution in case
     # the CSW fails to operate correctly while fetching, for example
-    rec_offset = 0
+    position = 1
     batches = 0
     while True:
         csw.getrecords2(outputschema=namespaces['gmd'],  # Return ISO 19115 metadata
-                        startposition=rec_offset,
+                        startposition=position,
                         esn='full', maxrecords=100)
         yield csw
 
-        if csw.results['matches'] == csw.results['nextrecord'] - 1 or \
-                batches >= max_batches:
+        # nextrecord is 0 when all matches have been exhausted according to
+        # the CSW 2.0.2 spec
+        if csw.results['nextrecord'] == 0 or batches >= max_batches:
             break
 
-        rec_offset = csw.results['nextrecord']
+        position = csw.results['nextrecord']
         batches += 1
 
 

commit 7674f9d0aba21032737f09364db6cd0afa3664cc

Handle non-conformant CSW nextrecord
Addresses issue introduced by 92394ea609db374175159407cbf910214f0e804c.
The aformentioned issue fixed an infinite loop condition if nextRecord
was 0, which appears to go along with the CSW spec.   However, some CSW
servers (tested against GeoNetwork 2.10.4) instead return a nextrecord
number which is greater than the number of matched records by one.
Handle these cases in the conditional statement so that both types of
CSW will work.

catalog_harvesting/csw.py
@@ -37,7 +37,12 @@ def get_records(csw, max_batches=10000):
 
         # nextrecord is 0 when all matches have been exhausted according to
         # the CSW 2.0.2 spec
-        if csw.results['nextrecord'] == 0 or batches >= max_batches:
+        if (csw.results['nextrecord'] == 0 or
+            # Some GeoNetwork implementations use nextrecord equal to
+            # matches + 1.  Cover this (non-standard?) case so that infinite
+            # loops don't occur
+            csw.results['nextrecord'] > csw.results['matches'] or
+            batches >= max_batches):
             break
 
         position = csw.results['nextrecord']